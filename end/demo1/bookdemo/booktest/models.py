02b5ee8576d6dff7c14ebfff8fad3457bbfc712a
lsh1001 <1987617587@qq.com>
2020-02-11 14:39:30 +0800

    创建项目创建应用

---
+++ end/demo1/bookdemo/booktest/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+
+# MVT M数据模型
+# ORM M数据模型
+# 在此处进行编写
---
+++ end/demo1/bookdemo/booktest/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+# MVT V视图模块
+# 在此处接受请求 处理数据 返回响应
