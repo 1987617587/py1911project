60fcfe617c5ac4c977175eaa380fcd5a30d60ce1
lsh1001 <1987617587@qq.com>
2020-02-13 17:25:59 +0800

    模板渲染

--- end/demo1/bookdemo/booktest/urls.py
+++ end/demo1/bookdemo/booktest/urls.py
@@ -6,7 +6,10 @@ from . import views
 # 2.每一个路由文件中 必须编写路由数组
 urlpatterns = [
     url(r'^index/$', views.index),
+    # 第一个参数就是路由 第二个是视图函数
     url(r'^list/$',views.list),
     url(r'^about/$',views.about),
+    # 使用正则分组 (\d+) 将正则分组匹配到的内容作为实参传递给视图函数
+    url(r'detail/(\d+)/',views.detail),
     # url(r'^detail/([0-9]+)/$', views.detail),
 ]
--- end/demo1/bookdemo/booktest/views.py
+++ end/demo1/bookdemo/booktest/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.template import loader
+from .models import Book,Hero
 # Create your views here.
 
 
@@ -8,12 +9,39 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # 3.在views.py中编写视图函数
 def index(request):
-    return HttpResponse("这里是首页")
 
+    # # return HttpResponse("这里是<h1>首页</h1>")
+    # # 1.获取模板
+    # template = loader.get_template('index.html')
+    # # 2.渲染模板数据
+    # books = Book.objects.all()
+    # context = {'books':books}
+    # result = template.render(context)
+    # # 3.将渲染结果使用httpresponse返回
+    # return HttpResponse(result)
+
+    # 快捷方式 (将上面3步简化操作)
+    books = Book.objects.all()
+    return render(request,'index.html',{'books':books})
 
 def list(request):
     return HttpResponse("这里是列表页")
 
+
+def detail(request,b_id):
+    # # return HttpResponse(f"这里{b_id}书是详情页")
+    # # 1.获取模板
+    # template = loader.get_template('detail.html')
+    # # 2.渲染模板数据
+    # book = Book.objects.get(id = b_id)
+    # context = {'book': book}
+    # result = template.render(context)
+    # # 3.将渲染结果使用httpresponse返回
+    # return HttpResponse(result)
+
+    # 快捷方式 (将上面3步简化操作)
+    book = Book.objects.get(id = b_id)
+    return render(request,'detail.html',{'book':book})
 
 def about(request):
     return HttpResponse("这里是关于页")
