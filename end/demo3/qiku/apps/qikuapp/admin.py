e2168f1d63f7a55223380493b64b9a9747892e91
lsh123 <1987617587@qq.com>
2020-02-24 12:08:16 +0800

    M

---
+++ end/demo3/qiku/apps/qikuapp/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+
+admin.site.register(Ads)
+admin.site.register(Category)
+admin.site.register(Teacher)
+admin.site.register(Comment)
---
+++ end/demo3/qiku/apps/qikuapp/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+
+# Create your models here.
+from DjangoUeditor.models import UEditorField
+
+
+class Ads(models.Model):
+    img = models.ImageField(upload_to="ads", verbose_name="图片")
+    desc = models.CharField(max_length=20, null=True, blank=True, verbose_name="图片说明")
+
+    class Mate:
+        verbose_name = "图片表"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=20, verbose_name="分类名")
+
+    def __str__(self):
+        return self.name
+
+    class Mate:
+        verbose_name = "分类表"
+
+
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=20, verbose_name="教师姓名")
+
+    field = models.CharField(max_length=50, verbose_name="权威领域")
+    category = models.ManyToManyField(Category, verbose_name="授课分类")
+    # body = models.TextField(verbose_name="正文")
+    # 使用富文本字段类型
+    introduce = UEditorField(imagePath="imgs/", width="100%", verbose_name="简介")
+    stars = models.PositiveIntegerField(default=3, verbose_name="评价星级")
+
+    def __str__(self):
+        return self.name
+
+    class Mate:
+        verbose_name = "教师表"
+
+
+class Comment(models.Model):
+    name = models.CharField(max_length=20, verbose_name="评论人")
+    url = models.URLField(default="http://www.zzy.com", verbose_name="个人主页")
+    email = models.EmailField(default="1862655@qq.com", verbose_name="个人邮箱")
+    body = models.TextField(verbose_name="评论内容")
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name="评论时间")
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, verbose_name="所属教师")
+
+    def __str__(self):
+        return self.name
+
+    class Mate:
+        verbose_name = "评论表"
