156f6bdc49b1fb78bc245a0706096f2cd09f42db
lsh123 <1987617587@qq.com>
2020-02-24 15:42:39 +0800

    M 课程

--- end/demo3/qiku/apps/qikuapp/models.py
+++ end/demo3/qiku/apps/qikuapp/models.py
@@ -22,12 +22,25 @@ class Category(models.Model):
         verbose_name = "分类表"
 
 
+class Curriculum(models.Model):
+    name = models.CharField(max_length=20, verbose_name="教师姓名")
+    category = models.ManyToManyField(Category, verbose_name="授课分类")
+    price = models.PositiveIntegerField(default=1000, verbose_name="课程费用")
+    days = models.PositiveIntegerField(default=7, verbose_name="课程时长")
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name="开课时间")
+    teacher = models.ForeignKey('Teacher', on_delete=models.CASCADE, verbose_name="授课教师")
+
+    def __str__(self):
+        return self.name
+
+    class Mate:
+        verbose_name = "课程表"
+
 
 class Teacher(models.Model):
-    name = models.CharField(max_length=20, verbose_name="教师姓名")
+    name = models.CharField(max_length=20, verbose_name="课程名")
 
     field = models.CharField(max_length=50, verbose_name="权威领域")
-    category = models.ManyToManyField(Category, verbose_name="授课分类")
     # body = models.TextField(verbose_name="正文")
     # 使用富文本字段类型
     introduce = UEditorField(imagePath="imgs/", width="100%", verbose_name="简介")
@@ -46,7 +59,7 @@ class Comment(models.Model):
     email = models.EmailField(default="1862655@qq.com", verbose_name="个人邮箱")
     body = models.TextField(verbose_name="评论内容")
     create_time = models.DateTimeField(auto_now_add=True, verbose_name="评论时间")
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, verbose_name="所属教师")
+    curriculum = models.ForeignKey(Curriculum, on_delete=models.CASCADE, verbose_name="所属课程")
 
     def __str__(self):
         return self.name
