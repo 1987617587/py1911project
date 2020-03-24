b0aac5b659ade067ff59d85ec1033ecc790d6744
lsh123 <1987617587@qq.com>
2020-03-24 11:59:53 +0800

    百度贴吧简单的数据抓取

--- pachong/PCdemo1/day01/task1/requests_double_chromosphere.py
+++ pachong/PCdemo1/day01/task1/requests_double_chromosphere.py
@@ -40,29 +40,61 @@ for item in ls:
         this_num = title
     elif i % 15 == 2:
         print(f"期号{this_num}开奖时间:", title)
+        pub_time = title
     elif i % 15 == 3:
         print(f"期号{this_num}红球1:", title)
+        red_bull1 = title
     elif i % 15 == 4:
         print(f"期号{this_num}红球2:", title)
+        red_bull2 = title
+
     elif i % 15 == 5:
         print(f"期号{this_num}红球3:", title)
+        red_bull3 = title
+
     elif i % 15 == 6:
         print(f"期号{this_num}红球4:", title)
+        red_bull4 = title
+
     elif i % 15 == 7:
         print(f"期号{this_num}红球5:", title)
+        red_bull5 = title
+
     elif i % 15 == 8:
         print(f"期号{this_num}红球6:", title)
+        red_bull6 = title
+
     elif i % 15 == 9:
         print(f"期号{this_num}篮球:", title)
+        blue_bull = title
     elif i % 15 == 10:
         print("期号{this_num}总投注额:", title)
+        sum_money = title
     elif i % 15 == 11:
         print(f"期号{this_num}一等奖注数:", title)
+        num1 = title
+
     elif i % 15 == 12:
         print(f"期号{this_num}一等奖奖金:", title)
+        num1_money = title
+
     elif i % 15 == 13:
         print(f"期号{this_num}二等奖注数:", title)
+        num2 = title
+
     elif i % 15 == 14:
         print(f"期号{this_num}二等奖奖金:", title)
+        num2_money = title
+
     else:
         print(f"期号{this_num}奖池滚存:", title)
+        sum_all = title
+    # with open("ssq.txt", 'a', encoding='utf-8') as file:
+    #     file.write(
+    #         this_num + '\t' + pub_time + '\t' + red_bull1 + '\t' + red_bull2 + '\t' + red_bull3 + '\t' + red_bull4 + '\t' + red_bull5 + '\t' + red_bull6 + '\t' + blue_bull + '\t' + sum_money + '\t' + num1 + '\t' + num1_money + '\t' + num2 + '\t' + num2_money + '\t' + sum_all)
+    with open("ssq.txt", 'a', encoding='utf-8') as file:
+
+        if i % 15 == 0:
+            file.write(title + '\n')
+        else:
+            file.write(title + '\t')
--- pachong/PCdemo1/hello.py
+++ pachong/PCdemo1/hello.py
@@ -1,1 +1,7 @@
 print("hello world")
+arr1 = [1, 2]
+arr2 = ["a", "b", "c", "b"]
+# for i in arr2:
+for i in range(len(arr2)):
+    print(arr2[i],arr1[i%2])
+
